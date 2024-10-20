9905abaddd0943693ecb482ab47c9da10f575526
Amir JCS <amirmahdyjdx@gmail.com>
2024-10-21 00:41:11 +0330

    pref: complete dark toggle styles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const _side_menu = document.querySelector("aside#side-menu");
 const _toggle = document.querySelector("button#menu-toggle");
 const _darkToggle = document.querySelector("button#dark-toggle");
+const _darkToggleLight = document.querySelector("button#dark-toggle > i-light");
+const _darkToggleDark = document.querySelector("button#dark-toggle > i-dark");
 _darkToggle.addEventListener("click", () => {
     document.documentElement.classList.toggle("dark");
+    _darkToggleLight.classList.toggle("hidden");
+    _darkToggleDark.classList.toggle("hidden");
 })
 
 _toggle.addEventListener("click", () => {
